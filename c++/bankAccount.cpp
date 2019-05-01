64b8000f8b4e005186b2bcabfbcf28e853b17712
chicshin <inchicshin@gmail.com>
2019-05-01 15:49:24 +0900

    update using class/this/copy constructor

--- c++/bankAccount.cpp
+++ c++/bankAccount.cpp
@@ -1,27 +1,71 @@
 #include <iostream>
 #include <cstring>
-
 using namespace std;
 const int NAME_LEN=20;
 
-//declare functions
 void ShowMenu(void);
 void CreateAccount(void);
 void DepositMoney(void);
 void WithdrawMoney(void);
-void ShowAllAccInfo(void);
+void ShowAllInfo(void);
 
-enum {MAKE=1, DEPOSIT, WITHDRAW, INQUIRE, EXIT};
+enum{CREATE=1, DEPOSIT, WITHDRAWL, INQUIRE, EXIT};
 
-typedef struct
+class Account
 {
     int accID;
     int balance;
-    char cusName[NAME_LEN];
-} Account;
+    char * cusName;
+public:
+    Account(int accID, int balance, char * cusName)
+    {
+        this->accID=accID;
+        this->balance=balance;
+        this->cusName=new char[strlen(cusName)+1];
+        strcpy(this->cusName, cusName);
+    }
 
-Account accArr[100];
-int accNum=0;    //number of accounts
+    Account(const Account & copy)
+    {
+        this->accID=copy.accID;
+        this->balance=copy.balance;
+        this->cusName=new char[strlen(copy.cusName)+1];
+        strcpy(this->cusName, copy.cusName);
+    }
+
+    int GetAccID() { return accID; }
+
+    void Deposit(int money)
+    {
+        balance+=money;
+    }
+
+    int Withdraw(int money)
+    {
+        if(balance<money)
+        {
+            return 0;
+        }
+        balance-=money;
+        return money;
+    }
+
+    void ShowAccInfo()
+    {
+        cout<<"ID: "<<accID<<endl;
+        cout<<"Name: "<<cusName<<endl;
+        cout<<"Balance: $"<<balance<<endl;
+    }
+
+    ~Account()
+    {
+        delete []cusName;
+    }
+};
+
+Account * accArr[100];
+int accNum=0;
+
 
 int main(void)
 {
@@ -30,28 +74,28 @@ int main(void)
     while(1)
     {
         ShowMenu();
-        cout<<"Select: ";
+        cout<<"Select... ";
         cin>>choice;
         cout<<endl;
 
         switch(choice)
         {
-            case MAKE:
-                CreateAccount();
-                break;
-            case DEPOSIT:
-                DepositMoney();
-                break;
-            case WITHDRAW:
-                WithdrawMoney();
-                break;
-            case INQUIRE:
-                ShowAllAccInfo();
-                break;
-            case EXIT:
-                return 0;
-            default:
-                cout<<"Illegal selection.."<<endl;
+        case CREATE:
+            CreateAccount();
+            break;
+        case DEPOSIT:
+            DepositMoney();
+            break;
+        case WITHDRAWL:
+            WithdrawMoney();
+            break;
+        case INQUIRE:
+            ShowAllInfo();
+            break;
+        case EXIT:
+            return 0;
+        default:
+            cout<<"Invalid Selection"<<endl;
         }
     }
     return 0;
@@ -59,12 +103,12 @@ int main(void)
 
 void ShowMenu(void)
 {
-    cout<<"-----MENU-----"<<endl;
+    cout<<"-----Menu-----"<<endl;
     cout<<"1. Create Account"<<endl;
-    cout<<"2. Deposit Money"<<endl;
-    cout<<"3. Withdraw Money"<<endl;
+    cout<<"2. Deposit"<<endl;
+    cout<<"3. Withdraw"<<endl;
     cout<<"4. Account Information"<<endl;
-    cout<<"5. Exit"<<endl;
+    cout<<"5. Quit"<<endl;
 }
 
 void CreateAccount(void)
@@ -73,70 +117,66 @@ void CreateAccount(void)
     char name[NAME_LEN];
     int balance;
 
-    cout<<"Create"<<endl;
-    cout<<"Account ID: "; cin>>id;
+    cout<<"Creating Account"<<endl;
+    cout<<"ID: "; cin>>id;
     cout<<"Name: "; cin>>name;
     cout<<"Balance: "; cin>>balance;
     cout<<endl;
 
-    accArr[accNum].accID=id;
-    accArr[accNum].balance=balance;
-    strcpy(accArr[accNum].cusName, name);
-    accNum++;
+    accArr[accNum++]=new Account(id, balance, name);
 }
 
 void DepositMoney(void)
 {
     int money;
     int id;
-    cout<<"Deposit"<<endl;
-    cout<<"Account ID: "; cin>>id;
-    cout<<"Amount of Money: "; cin>>money;
 
-    for (int i=0; i<accNum; i++)
+    cout<<"Deposit"<<endl;
+    cout<<"ID: "; cin>>id;
+    cout<<"Amount to deposit: $"; cin>>money;
+
+    for(int i=0; i<accNum; i++)
     {
-        if(accArr[i].accID==id)
+        if(accArr[i]->GetAccID()==id)
         {
-            accArr[i].balance+=money;
-            cout<<"Depoist Completed"<<endl;
+            accArr[i]->Deposit(money);
+            cout<<"Finishing Deposit"<<endl;
             return;
         }
     }
-    cout<<"Invalid ID"<<endl<<endl;
+    cout<<"Invalid ID"<<endl;
 }
 
 void WithdrawMoney(void)
 {
     int money;
     int id;
-    cout<<"Withdrawl"<<endl;
-    cout<<"Account ID: "; cin>>id;
-    cout<<"Amount of Money: "; cin>>money;
 
-    for (int i=0; i<accNum; i++)
+    cout<<"Withdrawl"<<endl;
+    cout<<"ID: "; cin>>id;
+    cout<<"Amount to with drawl: $"; cin>>money;
+
+    for(int i=0; i<accNum; i++)
     {
-        if(accArr[i].accID==id)
+        if(accArr[i]->GetAccID()==id)
         {
-            if(accArr[i].balance<money)
+            if(accArr[i]->Withdraw(money)==0)
             {
-                cout<<"Insufficient balance"<<endl;
+                cout<<"Balance Insufficient"<<endl;
                 return;
             }
-
-            accArr[i].balance-=money;
-            cout<<"Withdrawl Completed"<<endl;
+            cout<<"Finishing Withrawl"<<endl;
             return;
         }
     }
-    cout<<"Invalid ID"<<endl<<endl;
+    cout<<"Invalid Account ID"<<endl;
 }
 
-void ShowAllAccInfo(void)
+void ShowAllInfo(void)
 {
     for(int i=0; i<accNum; i++)
     {
-        cout<<"Account ID: "<<accArr[i].accID<<endl;
-        cout<<"Name: "<<accArr[i].cusName<<endl;
-        cout<<"Balance: "<<accArr[i].balance<<endl<<endl;
+        accArr[i]->ShowAccInfo();
+        cout<<endl;
     }
 }
