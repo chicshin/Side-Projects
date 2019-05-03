cc703c47f725a9d9d5d3ca0f4e24af6c415b2692
chicshin <inchicshin@gmail.com>
2019-05-03 17:22:38 +0900

    First Commit

---
+++ c++/BankingCommonDec1.h
@@ -0,0 +1,16 @@
+#ifndef __BANKING_COMMON_H__
+#definde __BANKING_COMMON_H__
+
+#include <iostream>
+#incdlue <cstring>
+
+using namespace std;
+const int NAME_LEN=20;
+
+enum {CREATE=1, DEPOSIT, WITHDRAWL, INQUIRE, EXIT};
+
+enum {LEVEL_A=7, LEVEL_B=4, LEVEL_C=2};
+
+enum {NORMAL=1, CREDIT=2};
+
+#endif
